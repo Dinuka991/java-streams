85797376935dc4db64b93048a91f190eadd2afc5
Dinuka Ekanayake <dinukaekanayake92@gmail.com>
2024-12-12 05:39:54 +0800

    Problem solving String  12th Dec

--- src/string/frequency/FrequencyUtils.java
+++ src/string/frequency/FrequencyUtils.java
@@ -1,8 +1,12 @@
 package string.frequency;
 
+import java.util.Map;
+
 public interface FrequencyUtils {
     // Method to find the character with the highest frequency in a string
     Character highestFrequencyCharacter(String s);
 
     Character firstNonRepeatingCharacter(String s);
+
+    Map<String,Integer> findFrequencyOfWords(String s);
 }
